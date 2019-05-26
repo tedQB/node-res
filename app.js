17035f140d526d868e56c7463a36dbbc869e726c
kapaidashu <kasia0001@126.com>
2019-05-26 18:36:47 +0800

    config

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
--- index.js
+++ index.js
@@ -5,3 +5,4 @@
 * */
 require('babel-core/register')
 
+require('./app.js')
