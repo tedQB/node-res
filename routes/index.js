0ae0db0e3a3ab326b429e929839ab00bf618aaa0
kapaidashu <kasia0001@126.com>
2019-05-26 12:19:20 +0800

    ??

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ExpressT' });
+  res.render('index', { title: 'ExpressTS' });
 });
 
 module.exports = router;
